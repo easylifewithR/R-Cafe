e1540060caba8d6d4aa3d9db787c07b42a41444f
Akash Gupta <akash.iitv@gmail.com>
2018-01-04 18:55:23 -0600

    Add files via upload
    Tutorial on R Shiny

---
+++ server.R
@@ -0,0 +1,21 @@
+library(shiny)
+shinyServer(function(input, output) {
+
+})
+
+
+# output$temperature = renderText({
+#   paste0("Temperature = ", input$temperature)
+# })
+# 
+# output$HR = renderText({
+#   paste0("Heart Rate = ", input$HR)
+# })
+# 
+# output$RR = renderText({
+#   paste0("Respiratory Rate = ", input$RR)
+# })
+# 
+# output$WBC = renderText({
+#   paste0("White Blood Cell count = ", input$WBC*1000)
+# })
